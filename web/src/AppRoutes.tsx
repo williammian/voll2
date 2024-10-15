861efb47f0b9f4ddf4e734d50fc3aa30e68da445
William Mian <william_mian@yahoo.com.br>
2024-10-15 17:01:10 -0300

    first commit

---
+++ web/src/AppRoutes.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import PaginaBase from "./pages/PaginaBase";
+import PaginaInicial from "./pages/PaginaInicial";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<PaginaBase />}>
+                <Route index element={<PaginaInicial />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
