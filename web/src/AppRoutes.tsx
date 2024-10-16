93cd1a845c24262cdf3417cd6054b2e537cef36e
William Mian <william_mian@yahoo.com.br>
2024-10-16 10:18:20 -0300

    construindo formularios

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -2,14 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import PaginaBase from "./pages/PaginaBase";
 import PaginaInicial from "./pages/PaginaInicial";
+import PaginaBaseFormulario from "./pages/PaginaBaseFormulario";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+
 
 function AppRoutes() {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<PaginaBase />}>
-                <Route index element={<PaginaInicial />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+            <Route index element={<PaginaInicial />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route path="/" element={<PaginaBaseFormulario />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
             </Route>
         </Routes>
         </BrowserRouter>
